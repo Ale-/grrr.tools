ce974cc7eb1892648c0e3cf2cb3311a94ee3665b
Ale <a@414c45.net>
2017-06-07 18:24:19 +0100

    feat: Add scaffold of Django project

---
+++ grrr/settings.py
@@ -0,0 +1,129 @@
+"""
+Django settings for GRRR
+"""
+
+import os
+from django.utils.translation import ugettext_lazy as _
+
+#
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_PATH = os.path.abspath( os.path.dirname(__file__) )
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ENV_PATH, '..', 'static')
+PROJECT_STATIC_FOLDER = 'grrr'
+STATICFILES_DIRS = [
+    ( PROJECT_STATIC_FOLDER, STATIC_ROOT + '/' + PROJECT_STATIC_FOLDER + '/' ),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ENV_PATH, '..', 'media')
+MAINTENANCE_IGNORE_URLS = (
+    r'^/admin/.*',
+    r'^/login$',
+)
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/logout'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_OPEN = True
+
+# Name of site in the document title
+DOCUMENT_TITLE = 'GRRR Tools'
+DOCUMENT_DESCRIPTION = _('Grupo para la reutilización y redistribución de recursos.')
+
+#
+# Application definition
+#
+
+CONTRIB_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+PROJECT_APPS = [
+    'apps.models',
+    'apps.users',
+    'apps.api',
+    'apps.views',
+    'apps.utils',
+]
+
+INSTALLED_APPS = CONTRIB_APPS + PROJECT_APPS
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'grrr.middleware.sqlite_settings_middleware.sqliteSettings' # To faster I/O in sqLite
+]
+
+ROOT_URLCONF = 'grrr.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'grrr.wsgi.application'
+
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+#
+# Import private settings
+#
+from .private_settings import *
---
+++ grrr/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+
+#if settings.DEBUG == True:
+  # urlpatterns += static( settings.STATIC_URL, document_root = settings.STATIC_ROOT )
+  # urlpatterns += static( settings.MEDIA_URL,  document_root = settings.MEDIA_ROOT )
