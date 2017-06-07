e3c9ebf24dd2462a7ceb5b8b276860b05140ffe4
Ale <a@414c45.net>
2017-06-07 19:47:34 +0100

    fix: Update wrong import of settings in Utils templatags

--- apps/utils/templatetags/utils.py
+++ apps/utils/templatetags/utils.py
@@ -1,29 +1,29 @@
 from django import template
 from django.utils.translation import ugettext_lazy as _
-from civics.settings import STATIC_URL, PROJECT_STATIC_FOLDER
+from django.conf import settings
 from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.simple_tag
 def css(file):
-    return  STATIC_URL + PROJECT_STATIC_FOLDER + '/css/' + file
+    return  settings.STATIC_URL + settings.PROJECT_STATIC_FOLDER + '/css/' + file
 
 @register.simple_tag
 def js(file):
-    return  STATIC_URL + PROJECT_STATIC_FOLDER + '/js/' + file
+    return  settings.STATIC_URL + settings.PROJECT_STATIC_FOLDER + '/js/' + file
 
 @register.simple_tag
 def angular(file):
-    return  mark_safe("<script type='text/javascript' src='" +STATIC_URL + PROJECT_STATIC_FOLDER + "/angular/" + file + "'></script>")
+    return  mark_safe("<script type='text/javascript' src='" + settings.STATIC_URL + settings.PROJECT_STATIC_FOLDER + "/angular/" + file + "'></script>")
 
 @register.simple_tag
 def img(file):
-    return  STATIC_URL + PROJECT_STATIC_FOLDER + '/img/' + file
+    return  settings.STATIC_URL + settings.PROJECT_STATIC_FOLDER + '/img/' + file
 
 @register.inclusion_tag('fake-breadcrumb.html')
 def fake_breadcrumb(text="Volver a la página anterior"):
-    return { 'text' : text }    
+    return { 'text' : text }
 
 @register.inclusion_tag('limited-choices-select.html')
 def limited_choices_select(choices=None, select_name=None, select_class=None, all=False, multiple=False):
