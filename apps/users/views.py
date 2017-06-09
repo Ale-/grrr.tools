1b3cd61623c5af7ce705248a325193423c01849d
Ale <a@414c45.net>
2017-06-09 19:10:33 +0100

    feat: Add dashboard template and view

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,16 @@
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+class UserAccount(View):
+    """Displays account of the current logged-in user."""
+
+    @method_decorator(login_required)
+    def get(self, request):
+        username       = request.user.username
+        date_joined    = request.user.date_joined
+        email          = request.user.email
+
+        return render(request, 'pages/dashboard.html', locals())
