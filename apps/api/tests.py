2c5711979fe94b1dbc9219491c54bb495dbd5aaa
Ale <a@414c45.net>
2017-06-18 22:19:14 +0100

    feat: Add nodes view to API

--- apps/api/tests.py
+++ apps/api/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import Client
+from django.test import TestCase
+from django.urls import reverse
+
+from apps.models import models
+
+class ApiTest(TestCase):
+    """ Test Api endpoints """
+
+    def test_nodes_endpoint(self):
+        """
+        Checks registration view and user registration
+        """
+        c = Client()
+        nodes_response = c.get(reverse('api:nodes_service'))
+        # Check empty view
+        self.assertEqual(nodes_response.status_code, 200)
+        self.assertEqual(nodes_response.content.decode("utf-8"), "If a tree falls in the woods and nobody is there to hear it, does it make a sound?")
+        # Check view with contents
+        test_post = models.Node.objects.create(
+            name = "Test node",
+            description  = "Lorem ipsum",
+            place        = "Test place",
+            address      = "Test address",
+            geom         = json.loads('{ "type": "Point", "coordinates": [0,0] }'),
+            members      = 0
+        )
+        nodes_response = c.get(reverse('api:nodes_service'))
+        content = nodes_response.json()
+        self.assertEqual(nodes_response.status_code, 200)
+        self.assertEqual(len(content), 1)
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse
+
+from apps.models import models
+
+
+def nodes(request):
+  """API endpoint to get all nodes in the site"""
+
+  nodes = models.Node.objects.all()
+  if len(nodes) > 0:
+      jsondump = []
+      for node in nodes:
+          if(node.geom):
+              item = {}
+              item['lat']   =  node.geom['coordinates'][1]
+              item['lon']   =  node.geom['coordinates'][0]
+              item['popup'] =  "<h5><a href='" + reverse('node', args=[node.slug]) + "'>" + node.name + "</a></h5>" + \
+                                   "<p>" + node.description + "</p>"
+          jsondump.append(item)
+      return HttpResponse(json.dumps(jsondump, indent=4), content_type="application/json")
+  else:
+      return HttpResponse("If a tree falls in the woods and nobody is there to hear it, does it make a sound?")
