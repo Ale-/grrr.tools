b8d1e813a1fc2e93340342a44c736e016191f505
Ale <a@414c45.net>
2017-06-23 09:09:33 +0100

    feat: Add an endpoint to display Reuses in apps/api

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
   url(r'^nodos$', views.nodes, name="nodes_service"),
+  url(r'^reusos$', views.reuses, name="reuses_service"),
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -24,3 +24,21 @@ def nodes(request):
       return HttpResponse(json.dumps(jsondump, indent=4), content_type="application/json")
   else:
       return HttpResponse("If a tree falls in the woods and nobody is there to hear it, does it make a sound?")
+
+def reuses(request):
+  """API endpoint to get all reuses in the site"""
+
+  nodes = models.Reuse.objects.all()
+  if len(nodes) > 0:
+      jsondump = []
+      for node in nodes:
+          if(node.geom):
+              item = {}
+              item['lat']   =  node.geom['coordinates'][1]
+              item['lon']   =  node.geom['coordinates'][0]
+              item['popup'] =  "<h5><a href='" + reverse('node', args=[node.slug]) + "'>" + node.name + "</a></h5>" + \
+                                   "<p>" + node.summary + "</p>"
+          jsondump.append(item)
+      return HttpResponse(json.dumps(jsondump, indent=4), content_type="application/json")
+  else:
+      return HttpResponse("If a tree falls in the woods and nobody is there to hear it, does it make a sound?")
