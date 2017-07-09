dd631f11ccc848c89ce0238da80de8b880587036
Ale <a@414c45.net>
2017-07-09 21:36:52 +0100

    feat: Add UserProfile

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    last_message_seen_date = models.DateField()
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, **kwargs):
+    if kwargs.get('created', False):
+        UserProfile.objects.get_or_create(user=kwargs.get('instance'))
