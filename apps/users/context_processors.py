7913ae29e99db3b8b2177bc59d812f43e2a418e1
Ale <a@414c45.net>
2017-07-12 15:14:51 +0100

    fix: Fix Hello unseen messages context processor to not break with anonymous users

--- apps/users/context_processors.py
+++ apps/users/context_processors.py
@@ -5,9 +5,11 @@ from apps.users.models import UserProfile
 def unseen_messages(request):
     """Injects into global context number of user's unseen messages"""
 
-    nodes            = models.Node.objects.filter(users__in=[request.user])
-    spaces           = models.Space.objects.filter(nodes__in=nodes)
-    profile_datetime = get_object_or_404(UserProfile, user=request.user).last_message_seen_datetime
-    sms              = models.Sms.objects.filter(receiver__in=spaces, datetime__gt=profile_datetime).count()
+    if request.user.is_authenticated:
+        nodes            = models.Node.objects.filter(users__in=[request.user])
+        spaces           = models.Space.objects.filter(nodes__in=nodes)
+        profile_datetime = get_object_or_404(UserProfile, user=request.user).last_message_seen_datetime
+        sms              = models.Sms.objects.filter(receiver__in=spaces, datetime__gt=profile_datetime).count()
+        return { 'sms' : sms }
 
-    return { 'sms' : sms }
+    return { 'sms' : 0 }
